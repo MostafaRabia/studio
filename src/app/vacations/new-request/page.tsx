563394fb6e9ae2cc963d275ce19502f58ce800e5
Mohamed Mohamed <chainnest2022@gmail.com>
2025-05-29 21:01:58 +0000

    add a new button in this page named" new vacation request"

--- src/app/vacations/new-request/page.tsx
+++ src/app/vacations/new-request/page.tsx
@@ -1,7 +1,7 @@
 
 import { PageHeader } from '@/components/page-header';
 import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, PlusCircle } from 'lucide-react'; // Added PlusCircle
 import Link from 'next/link';
 
 export default function NewVacationRequestPage() {
@@ -11,12 +11,21 @@ export default function NewVacationRequestPage() {
         title="New Vacation Request"
         description="Fill out the form below to submit your vacation request."
         actions={
-          <Link href="/vacations" passHref>
-            <Button variant="outline">
-              <ArrowLeft className="mr-2 h-4 w-4" />
-              Back to Vacations
-            </Button>
-          </Link>
+          <div className="flex flex-col sm:flex-row gap-2"> {/* Wrapper for buttons */}
+            <Link href="/vacations" passHref>
+              <Button variant="outline">
+                <ArrowLeft className="mr-2 h-4 w-4" />
+                Back to Vacations
+              </Button>
+            </Link>
+            {/* New Button Added Here */}
+            <Link href="/vacations/new-request" passHref> {/* Links to itself */}
+              <Button>
+                <PlusCircle className="mr-2 h-4 w-4" />
+                New Vacation Request
+              </Button>
+            </Link>
+          </div>
         }
       />
       <div className="flex items-center justify-center h-64 border-2 border-dashed rounded-md">
